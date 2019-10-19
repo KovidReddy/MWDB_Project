e29dff17d79554f2e68d39984a56f5206b933252
Vishal Tyagi <vishaltyagi081194@gmail.com>
2019-10-19 16:33:32 -0700

    Added Task 1 Visusalizer and merged all branches

--- task1.py
+++ task1.py
@@ -1,21 +1,29 @@
 from dimReduction import dimReduction
-
-arg = input("Please enter the home directory for the images "
-            "(Default: C:\\Users\\pylak\\Documents\\Fall 2019\\MWDB\\Project\\Phase1\\Hands_test2\\) : ")
-if arg == '':
-    arg = 'C:\\Users\\pylak\\Documents\\Fall 2019\\MWDB\\Project\\Phase1\\Hands_test2\\'
-dim = dimReduction(arg, '*.jpg')
-arg2 = input('Please choose a feature model - SIFT(s), Moments(m), LBP(l), Histogram(h): ')
-if arg2 not in ('s', 'm', 'l', 'h'):
+import os
+spath = input("Please enter the home directory for the images "
+            "(Default: C:\\ASU\\Fall 2019\\MWDB\\Project\\Phase 2\\Dataset2\\) : ")
+if spath == '':
+    spath = 'C:\\ASU\\Fall 2019\\MWDB\\Project\\Phase 2\\Dataset2\\'
+dim = dimReduction(spath, '*.jpg')
+feature = input('Please choose a feature model - SIFT(s), Moments(m), LBP(l), Histogram(h): ')
+if feature not in ('s', 'm', 'l', 'h'):
     print('Please enter a valid feature model!')
     exit()
-arg3 = input('Please choose a dimensionality reduction technique - PCA(pca), SVD(svd), NMF(nmf), LDA(lda)')
-arg4 = input('Please provide the number of latent semantics(k): ')
-db = 'imagedata_' + arg2 + '_' + arg3
-imgs_sort, feature_sort = dim.saveDim(arg2, arg3, db, int(arg4))
+technique = input('Please choose a dimensionality reduction technique - PCA(pca), SVD(svd), NMF(nmf), LDA(lda): ')
+k = input('Please provide the number of latent semantics(k): ')
+db = 'imagedata_' + feature
+imgs_sort, feature_sort = dim.saveDim(feature, technique, db, int(k), password="password", database="mwdb")
+path = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + 'Outputs'  +os.sep)
+print(path)
 print('\n')
 print('Data Latent Semantics Saved to Output Folder!')
-dim.writeFile(imgs_sort, 'Output\\Task1\\Data_ls_{x}_{y}_{z}.txt'.format(x=arg2,y=arg3,z=arg4))
+if not os.path.exists(path):
+    os.makedirs(path)
+dim.writeFile(imgs_sort, path + os.sep + 'Task1_Data_ls_{x}_{y}_{z}.txt'.format(x=feature,y=technique,z=k))
+# print(imgs_sort)
 print('\n')
 print('Feature Latent Semantics Saved to Output Folder!')
-dim.writeFile(imgs_sort, 'Output\\Task1\\Feature_ls_{x}_{y}_{z}.txt'.format(x=arg2,y=arg3,z=arg4))
+dim.writeFile(feature_sort, path + os.sep + 'Task1_Feature_ls_{x}_{y}_{z}.txt'.format(x=feature,y=technique,z=k))
+# print(feature_sort)
+dim.imgViz(imgs_sort, spath)
+dim.imgViz_feature(feature_sort, spath)
